ed3b0221cc1754fcaabf47bb476b75300234041a
Cassidy Wenmoth <c.wenmoth@gmail.com>
2020-07-07 19:18:56 +1200

    Hello World

--- My First Project/HelloWorld.java
+++ My First Project/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Output Hello World to the user
 		System.out.println("Hello World");
 	}
 
